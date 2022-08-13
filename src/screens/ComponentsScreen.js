01ba53ad05c4fd3cd575576f8af194635b2d5aa6
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-08-13 11:10:42 -0400

    getting started

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the components screen</Text>;
+  const Greeting = <Text style={styles.textStyle2}>My Name is Anthony</Text>;
+
+  return (
+    <View>
+      <Text style={styles.textStyle}>Getting Started with Reatc Native</Text>
+      {Greeting}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30
-    }
+  textStyle: {
+    fontSize: 45,
+  },
+  textStyle2: {
+    fontSize: 20,
+  },
 });
 
 export default ComponentsScreen;
