28715f18c79522e6abe1227daa98e51744ad63f1
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-08-13 17:24:03 -0400

    refactor counter demo screen to use reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,28 +1,43 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const reducer = (state, action) => {
+  // state === {count: number}
+  // action === {type: increase || decrease, payload: 1}
+
+  switch (action.type) {
+    case "change_increase":
+      return { ...state, count: state.count + action.payload };
+    case "change_decrease":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   return (
     <View>
       <Button
         title="increase"
-        onPress={() => {
-          setCount(count + 1);
-        }}
+        onPress={() => dispatch({ type: "change_increase", payload: 1 })}
       />
       <Button
         title="decrease"
-        onPress={() => {
-          setCount(count - 1);
-        }}
+        onPress={() => dispatch({ type: "change_decrease", payload: 1 })}
       />
-      <Text>Current Count: {count}</Text>
+      <Text style={styles.text}>Current Count: {state.count}</Text>
     </View>
   );
 };
 
-styles = StyleSheet.create({});
+styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    textAlign: "center",
+  },
+});
 
 export default CounterScreen;
